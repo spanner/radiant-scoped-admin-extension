8d13830d6051b6bd41fce78b78bdd4ebdd32d917
will <will@spanner.org>
2009-02-24 12:38:20 +0000

    site cookie set in admin for consistency (in index) and retrieval (in edit and update)

--- lib/scoped_admin/scoped_controller.rb
+++ lib/scoped_admin/scoped_controller.rb
@@ -2,29 +2,40 @@ module ScopedAdmin
   module ScopedController
     def self.included(base)
       base.class_eval {
-        alias_method_chain :current_user, :site
+        alias_method_chain :authenticate, :reset_site
       }
     end
 
-    # we don't want to process site and root parameters until we know this is an admin user
+    # we don't want to process site and root parameters until we know this is an entitled user
     # so we may have to go back and reset current_site after authentication
-    # non-admin users will be confined to the site they log into, and unable to log into anything but their own
+    # we could really do with some hooks here
   
-    def current_user_with_site
-      user = current_user_without_site
-      if user && user.admin?
-
+    def authenticate_with_reset_site
+      outcome = authenticate_without_reset_site
+      if outcome && !current_user.site
         if params[:site] && site = Site.find(params[:site])          
           @site = self.current_site = site
-          
         elsif params[:root] && page = Page.find(params[:root])
           @site = self.current_site = page.root.site
+        elsif site = site_from_cookie
+          @site = self.current_site = site
         end
-        
-        set_current_site if @site
+        if @site
+          set_current_site 
+          set_site_cookie
+        end
       end
-      user
+      outcome
     end
   
+    def set_site_cookie
+      cookies[:site_id] = { :value => current_site.id.to_s }
+    end
+    
+    def site_from_cookie
+      if !cookies[:site_id].blank? && site = Site.find(cookies[:site_id])
+        site
+      end
+    end
   end
 end
--- spec/controllers/admin/snippets_controller_spec.rb
+++ spec/controllers/admin/snippets_controller_spec.rb
@@ -84,12 +84,36 @@ describe Admin::SnippetsController do
       end
     end
   
-    describe "with site parameters" do
-      it "should ignore them" do
-        get :index, :site => site_id(:site2), :root => page_id(:home2)
-        Page.current_site.should == sites(:default)
+    describe "with a site cookie" do
+      before do
+        @cookies = { :site_id => site_id(:site2) }
+        controller.should_receive(:cookies).at_least(:once).and_return(@cookies)
+      end
+      
+      it "should set current_site to that site" do
+        get :index
+        Page.current_site.should == sites(:site2)
       end
     end
+
+    describe "with a site parameter" do
+      
+      it "should set current_site to that site" do
+        get :index, :site => site_id(:site2)
+        Page.current_site.should == sites(:site2)
+      end
+
+      it "should set a site_id cookie" do
+        @cookies = {}
+        controller.stub!(:cookies).and_return(@cookies)
+        @cookies.should_receive(:[]=) do |name,content|
+          name.should eql(:site_id)
+          content[:value].should == site_id(:site2).to_s
+        end
+        get :index, :site => site_id(:site2)
+      end
+    end
+
   end
   
   
