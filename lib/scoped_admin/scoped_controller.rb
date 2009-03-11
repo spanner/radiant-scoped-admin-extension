369323e158f5e7951298b2933aad1962303663dd
will <will@spanner.org>
2009-03-11 12:56:26 +0000

    brought site-setter up to date

--- lib/scoped_admin/scoped_controller.rb
+++ lib/scoped_admin/scoped_controller.rb
@@ -13,7 +13,7 @@ module ScopedAdmin
     def authenticate_with_reset_site
       outcome = authenticate_without_reset_site
       if outcome && !current_user.site
-        if params[:site] && site = Site.find(params[:site])          
+        if params[:site_id] && site = Site.find(params[:site_id])          
           @site = self.current_site = site
         elsif params[:root] && page = Page.find(params[:root])
           @site = self.current_site = page.root.site
@@ -28,14 +28,5 @@ module ScopedAdmin
       outcome
     end
   
-    def set_site_cookie
-      cookies[:site_id] = { :value => current_site.id.to_s }
-    end
-    
-    def site_from_cookie
-      if !cookies[:site_id].blank? && site = Site.find(cookies[:site_id])
-        site
-      end
-    end
   end
 end
